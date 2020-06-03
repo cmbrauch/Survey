7b6e5c3af452ed7136a1d5873663f97b44d93040
Chris <cmbrauch00@gmail.com>
2020-06-03 18:12:56 -0400

    create /css /js index.html scripts.js README.md and add a body to our index.html
    co-authored-by Mike Watts<WattsJMichael@gmail.com>

---
+++ practice.js
@@ -0,0 +1,8 @@
+let name = prompt("Enter your name"); 
+let food = prompt("What's your favorite food?");
+let age = prompt("What is your age?");
+
+function newSentence(whatToSay) {
+alert(whatToSay);
+};
+newSentence("Hello, my name is " + name + ", my favorite food is" + food + " , and I am " + age + "years old!");
